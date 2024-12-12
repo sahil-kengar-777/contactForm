64511579c8209f0c9193aeef808f5009b476ed1f
sahil-kengar-777 <sahilkengar7@gmail.com>
2024-12-12 14:05:41 +0530

    Added assignment 13

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -2,23 +2,33 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  providers : [ FormBuilder, Validators],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
 
-  ContactForm = new FormGroup(
+  obj = new FormBuilder();
+  displayFlag = false;
+  ContactForm = this.obj.group(
   {
-    firstName : new FormControl(''),
-    lastName : new FormControl(''),
-    email : new FormControl(''),
-    phone : new FormControl(''),
-    address : new FormControl('')
+    firstName: ['',[Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
+    lastName: ['',[Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
+    email: ['',[Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z]{1,}\.[a-zA-Z]{2,}$')]],
+    phone : ['',[Validators.required,Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]],
+    City: ['',[Validators.required, Validators.pattern('^(.*?[a-zA-Z]){4,}.*$')]],
+    state: ['',[Validators.required]],
+    zipcode: ['', [Validators.required, Validators.pattern(/^\d{5}$/)]],
+    comment: ['',[Validators.required,Validators.minLength(30)]]
   });
 
+  showData(){
+    this.displayFlag = true;
+  }
 }
